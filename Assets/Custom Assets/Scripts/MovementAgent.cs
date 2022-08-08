f549420932086f8bd5fb959d647e19fc891c40b4
PlipiKunz <johnathankunz@gmail.com>
2022-08-08 15:25:56 -0600

    update version

--- Assets/Custom Assets/Scripts/MovementAgent.cs
+++ Assets/Custom Assets/Scripts/MovementAgent.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
 
 public class MovementAgent : Agent
 {
@@ -22,19 +23,20 @@ public class MovementAgent : Agent
         sensor.AddObservation(targetTransform.localPosition);
     }
 
-    public override void OnActionReceived(float[] vectorActions)
+    public override void Heuristic(in ActionBuffers actionsOut)
     {
-        float moveX = vectorActions[0];
+        /*actionsOut[1] = Input.GetAxisRaw("Horizontal");
+        actionsOut[0] = -Input.GetAxisRaw("Vertical");*/
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+       /* float moveX = vectorActions[0];
         float moveZ = vectorActions[1];
 
-        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;*/
     }
 
-    public override void Heuristic(float[] actionsOut)
-    {
-        actionsOut[1] = Input.GetAxisRaw("Horizontal");
-        actionsOut[0] = -Input.GetAxisRaw("Vertical");
-    }
 
     private void OnTriggerEnter(Collider other)
     {
