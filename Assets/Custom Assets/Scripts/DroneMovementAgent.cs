1de997e9e10f8a6d70db51c8c96b5148985b73d8
PlipiKunz <johnathankunz@gmail.com>
2022-08-14 01:45:07 -0600

    Set up randomization of scene and inputs.

---
+++ Assets/Custom Assets/Scripts/DroneMovementAgent.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class DroneMovementAgent : Agent
+{
+    float moveSpeed = 5;
+    [SerializeField] private Transform targetTransform = null;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = Vector3.zero;
+        targetTransform.localPosition = new Vector3(Random.Range(-9f, 9f), Random.Range(5, 19f), Random.Range(-9f, 9f));
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(float[] vectorActions)
+    {
+        float moveX = vectorActions[0];
+        float moveZ = vectorActions[1];
+        float moveY = vectorActions[2];
+
+        transform.localPosition += new Vector3(moveX, moveY, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[2] = -Input.GetAxisRaw("UpDown");
+        actionsOut[1] = Input.GetAxisRaw("Horizontal");
+        actionsOut[0] = -Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Goal")
+        {
+            SetReward(1f);
+        }
+        else if (other.tag == "Wall") { 
+            SetReward(-1f);
+        }
+        EndEpisode();
+    }
+}
